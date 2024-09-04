546d1337facf84f6cec548f9a033df93e41ed5bb
Aadish Jain <aadishjain017@gmail.com>
2024-09-04 23:20:00 +0530

    installed react and tailwind css and did basic navbar thing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './components/navbar'
+import Team from './components/team'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Team />
+    </>
+  )
+}
+
+export default App
