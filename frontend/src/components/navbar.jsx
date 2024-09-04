9791f2ed6d5c9513e59250b51c98a0f04a002752
Aadish Jain <aadishjain017@gmail.com>
2024-09-05 00:20:50 +0530

    improved navbar and team template

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,44 +1,136 @@
-import React from 'react'
-import logo_img from '../assets/logo_bg_remove.png'
+import React, { useState } from 'react';
+import logo_img from '../assets/logo_bg_remove.png';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <div class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src={logo_img} class="h-8" alt="Flowbite Logo" />
-                    {/* <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Anveshan</span> */}
-                </div>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                    </svg>
-                </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                    <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                    </li>
-                    <li>
-                    <a href="#team" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Team</a>
-                    </li>
-                    <li>
-                    <a href="#events" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Events</a>
-                    </li>
-                    <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Achievements</a>
-                    </li>
-                    <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
-                    </li>
-                </ul>
-                </div>
+    <nav className="bg-gray-800 shadow-lg">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded={isOpen ? 'true' : 'false'}
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
+            <div className="flex-shrink-0">
+              <img className="h-12 w-auto p-2" src={logo_img} alt="Workflow" />
             </div>
-        </nav>
-    </div>
-  )
+            <div className="hidden sm:block sm:ml-6 flex-1">
+              <div className="flex justify-center space-x-4">
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Team
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Events
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Achievements
+                </a>
+              </div>
+            </div>
+            <div className="hidden sm:block">
+              <a
+                href="#"
+                className="text-white bg-blue-600 hover:bg-blue-700 px-4 py-2  rounded-md text-sm font-medium"
+              >
+                Contact Us
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1 text-center">
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Team
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Events
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Achievements
+          </a>
+          <a
+            href="#"
+            className="text-white bg-blue-600 hover:bg-blue-700 block px-3 py-2 rounded-md text-base font-medium text-center"
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/team.jsx
+++ frontend/src/components/team.jsx
@@ -3,36 +3,111 @@ import Aadish from '../assets/team-photos/Aadish.jpg'
 
 function Team() {
   return (
-    <div>
-        <div>
-            <h1 className='text-5xl text-center m-5'>Senior Council</h1>
-            <div id="team" className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 text-center mx-16 my-10 px-20 py-10'>
-                <img src="" class="w-56 h-96 object-cover" alt="President Neha" />
-                <img src="" class="w-56 h-96 object-cover" alt="Vice President Parth" />
-                <img src={Aadish} class="w-56 h-96 object-cover" alt="Vice President Aadish" />
-                <img src="" class="w-56 h-96 object-cover" alt="Head of Operations Alka" />
-                <img src="" class="w-56 h-96 object-cover" alt="Secretary Vaibhav" />
-                <img src="" class="w-56 h-96 object-cover" alt="Social Media Head Shruti" />
-                <img src="" class="w-56 h-96 object-cover" alt="Graphics Head Kaushal" />
-                <img src="" class="w-56 h-96 object-cover" alt="Graphics Head Surender" />
-                <img src="" class="w-56 h-96 object-cover" alt="PR Head Aryaman" />
-                <img src="" class="w-56 h-96 object-cover" alt="DSA Head Bhavishya" />
-                <img src="" class="w-56 h-96 object-cover" alt="Web Head Tanay" />
-                <img src="" class="w-56 h-96 object-cover" alt="Android Head Aditya" />
+    <div className='bg-gray-100 min-h-screen'>
+        <div className='py-12'>
+            <h1 className='text-5xl text-center font-bold mb-10'>Senior Council</h1>
+            <div id="team" className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 justify-items-center text-center'>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="President Neha" />
+                    <p className="mt-4 text-xl font-semibold">Neha</p>
+                    <p className="text-gray-500">President</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Vice President Parth" />
+                    <p className="mt-4 text-xl font-semibold">Parth</p>
+                    <p className="text-gray-500">Vice President</p>
+                </div>
+                <div className='team-member'>
+                    <img src={Aadish} className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Vice President Aadish" />
+                    <p className="mt-4 text-xl font-semibold">Aadish</p>
+                    <p className="text-gray-500">Vice President</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Head of Operations Alka" />
+                    <p className="mt-4 text-xl font-semibold">Alka</p>
+                    <p className="text-gray-500">Head of Operations</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Secretary Vaibhav" />
+                    <p className="mt-4 text-xl font-semibold">Vaibhav</p>
+                    <p className="text-gray-500">Secretary</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Social Media Head Shruti" />
+                    <p className="mt-4 text-xl font-semibold">Shruti</p>
+                    <p className="text-gray-500">Social Media Head</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Graphics Head Kaushal" />
+                    <p className="mt-4 text-xl font-semibold">Kaushal</p>
+                    <p className="text-gray-500">Graphics Head</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Graphics Head Surender" />
+                    <p className="mt-4 text-xl font-semibold">Surender</p>
+                    <p className="text-gray-500">Graphics Head</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="PR Head Aryaman" />
+                    <p className="mt-4 text-xl font-semibold">Aryaman</p>
+                    <p className="text-gray-500">PR Head</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="DSA Head Bhavishya" />
+                    <p className="mt-4 text-xl font-semibold">Bhavishya</p>
+                    <p className="text-gray-500">DSA Head</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Web Head Tanay" />
+                    <p className="mt-4 text-xl font-semibold">Tanay</p>
+                    <p className="text-gray-500">Web Head</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Android Head Aditya" />
+                    <p className="mt-4 text-xl font-semibold">Aditya</p>
+                    <p className="text-gray-500">Android Head</p>
+                </div>
             </div>
         </div>
-        <div>
-            <h1 className='text-5xl text-center m-5'>Junior Council</h1>
-            <div id="team" className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 text-center mx-16 my-10 px-20 py-10'>
-                <img src="" class="w-56 h-96 object-cover" alt="Shamit Gupta" />
-                <img src="" class="w-56 h-96 object-cover" alt="Gauri Behl" />
-                <img src="" class="w-56 h-96 object-cover" alt="Raushan" />
-                <img src="" class="w-56 h-96 object-cover" alt="Vanshika Tanwar" />
-                <img src="" class="w-56 h-96 object-cover" alt="Astha Negi" />
-                <img src="" class="w-56 h-96 object-cover" alt="Shwet Kashyap" />
-                <img src="" class="w-56 h-96 object-cover" alt="Preeti Kumari" />
-                <img src="" class="w-56 h-96 object-cover" alt="Riya Verma" />
-                <img src="" class="w-56 h-96 object-cover" alt="Keshav Mehra" />
+        <div className='py-12 bg-gray-200'>
+            <h1 className='text-5xl text-center font-bold mb-10'>Junior Council</h1>
+            <div id="team" className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 justify-items-center'>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Shamit Gupta" />
+                    <p className="mt-4 text-xl font-semibold">Shamit Gupta</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Gauri Behl" />
+                    <p className="mt-4 text-xl font-semibold">Gauri Behl</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Raushan" />
+                    <p className="mt-4 text-xl font-semibold">Raushan</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Vanshika Tanwar" />
+                    <p className="mt-4 text-xl font-semibold">Vanshika Tanwar</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Astha Negi" />
+                    <p className="mt-4 text-xl font-semibold">Astha Negi</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Shwet Kashyap" />
+                    <p className="mt-4 text-xl font-semibold">Shwet Kashyap</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Preeti Kumari" />
+                    <p className="mt-4 text-xl font-semibold">Preeti Kumari</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Riya Verma" />
+                    <p className="mt-4 text-xl font-semibold">Riya Verma</p>
+                </div>
+                <div className='team-member'>
+                    <img src="" className="w-56 h-96 object-cover rounded-lg shadow-lg transition-transform transform hover:scale-105" alt="Keshav Mehra" />
+                    <p className="mt-4 text-xl font-semibold">Keshav Mehra</p>
+                </div>
             </div>
         </div>
     </div>
