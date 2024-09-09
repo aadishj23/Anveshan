cd4c61131de99508b683e27eb6e22d31d5f7ba20
Aadish Jain <aadishjain017@gmail.com>
2024-09-09 20:04:29 +0530

    added basic backend files

--- backend/database.js
+++ backend/database.js
@@ -11,3 +11,13 @@ mongoose.connect(URL).then(() => {
     console.log('Error connecting to the database');
     console.log(err);
 });
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const Contact = require('./database');
 
 app.use(express.json());
+dotenv.config();
 
-app.post 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
