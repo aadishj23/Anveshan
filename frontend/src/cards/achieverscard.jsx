a7d16848302def8a7c0090c41e6cf05e3e3d8552
Aadish Jain <aadishjain017@gmail.com>
2024-09-10 13:35:31 +0530

    added vercel.json file and improved mobile view of achievers page

--- frontend/src/cards/achieverscard.jsx
+++ frontend/src/cards/achieverscard.jsx
@@ -11,9 +11,9 @@ function Achieverscard({ name, image,batch, role, exrole , achieve}) {
         <div className="p-6">
             <h3 className="text-2xl font-semibold text-gray-800">{name}</h3>
             <p className="mt-2 text-lg text-gray-500">{batch}</p>
-            <p className="mt-2 text-lg text-gray-500">{role}</p>
-            {exrole && <p className="mt-2 text-lg text-gray-500">{`Ex- ${exrole}`}</p>}
-            {achieve && <p className="mt-2 text-lg text-gray-500">{`Achievements: ${achieve}`}</p>}
+            <p className=" max-w-72 mt-2 text-lg text-gray-500">{role}</p>
+            {exrole && <p className=" max-w-72 mt-2 text-lg text-gray-500">{`Ex- ${exrole}`}</p>}
+            {achieve && <p className="max-w-72 mt-2 text-lg text-gray-500">{`Achievements: ${achieve}`}</p>}
         </div>
     </div>
   )
