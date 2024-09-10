a38f4c7dae2de43f221d68925f949cee6010b5c6
Aadish Jain <aadishjain017@gmail.com>
2024-09-10 13:39:55 +0530

    Improved achievement section mobile view

--- frontend/src/cards/achieverscard.jsx
+++ frontend/src/cards/achieverscard.jsx
@@ -4,16 +4,16 @@ function Achieverscard({ name, image,batch, role, exrole , achieve}) {
   return (
     <div className="max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 transform hover:scale-105">
         <img
-            className="w-72 h-64 object-cover rounded-t-lg"
+            className="w-80 h-64 object-cover rounded-t-lg"
             src={image}
             alt={`${name}'s photo`}
         />
         <div className="p-6">
             <h3 className="text-2xl font-semibold text-gray-800">{name}</h3>
             <p className="mt-2 text-lg text-gray-500">{batch}</p>
-            <p className=" max-w-72 mt-2 text-lg text-gray-500">{role}</p>
-            {exrole && <p className=" max-w-72 mt-2 text-lg text-gray-500">{`Ex- ${exrole}`}</p>}
-            {achieve && <p className="max-w-72 mt-2 text-lg text-gray-500">{`Achievements: ${achieve}`}</p>}
+            <p className=" max-w-64 mt-2 text-lg text-gray-500">{role}</p>
+            {exrole && <p className=" max-w-64 mt-2 text-lg text-gray-500">{`Ex- ${exrole}`}</p>}
+            {achieve && <p className="max-w-64 mt-2 text-lg text-gray-500">{`Achievements: ${achieve}`}</p>}
         </div>
     </div>
   )
