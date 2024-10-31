ffa40c327c1ea184d3cc7727c96c05f26c19e5ce
Aadish Jain <aadishjain017@gmail.com>
2024-10-31 15:26:19 +0530

    changed some code that might create merge conflicts

--- frontend/src/cards/Project/ProjectCard2.jsx
+++ frontend/src/cards/Project/ProjectCard2.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function ProjectCard2({image, work , description}) {
   return (
-    <div className='text-white border rounded-lg border-slate-400 h-96 w-fit mb-10 hidden sm:block shadow-2xl'>
+    <div className='text-white border rounded-lg border-slate-400 h-96 w-fit mb-10 hidden sm:block'>
       <div className="relative w-full h-full">
         <div
-          className="absolute inset-0 bg-cover bg-center rounded-lg"
+          className="absolute inset-0 bg-cover bg-center"
           style={{
             backgroundImage: `url(${image})`,
           }}
         ></div>
-      <div className="absolute inset-0 bg-gradient-to-t from-orange-500 via-transparent to-transparent opacity-65  rounded-lg"></div>
+      <div className="absolute inset-0 bg-gradient-to-t from-orange-500 via-transparent to-transparent opacity-65"></div>
       <div className="relative z-10 flex items-end h-full text-left">
         <div className="text-white px-4">
           <h1 className="font-bold mb-2">{work}</h1>
