4428f8e52d0ddf750051a6d36cb24000173d07a9
Aadish Jain <aadishjain017@gmail.com>
2024-11-04 23:34:50 +0530

    changed order of events

--- frontend/src/data/EventsData.js
+++ frontend/src/data/EventsData.js
@@ -8,6 +8,20 @@ import FrazImage from '../assets/event-photos/Succesfull Engineer ft Fraz/2.png'
 
 const EventsData= [
     {
+        "id": 5,
+        "name": "Tech Starter 2k24",
+        "description": "Tech Starter is Anveshan's month-long foundational cohort, specially designed to equip students with essential skills across core tech domains. Led by our Core Team members, this immersive program included hands-on sessions on HTML & CSS, Git and GitHub, JavaScript, C++, Java, and an Introduction to Data Structures and Algorithms.Each session was crafted to provide a practical, beginner-friendly approach, allowing students to build confidence and solidify their understanding of these foundational topics. Tech Starter empowers participants with the knowledge and skills they need to embark on their tech journeys, whether they're diving into web development, coding fundamentals, or programming logic.",
+        "date": "14-09-2024 to 17-10-2024",
+        "image": TechStarterImage,
+    },
+    {
+        "id": 3,
+        "name": "Meet Your Alumni: Sawarnee Sethi",
+        "description": "Meet Your Alumni is a special event by Anveshan, designed to connect BPIT students with accomplished alumni for invaluable career guidance. Recently, we hosted Mr. Sawarnee Sethi, a Cloud Engineer at AWS, who shared his expertise on Internships and Placements.In this session, Mr. Sethi offered strategies for navigating the internship and placement process, preparing for interviews, and building a standout profile. With his firsthand industry insights, students gained practical knowledge on transitioning from academic life to a successful tech career.", 
+        "date": "18-03-2024",
+        "image": AlumImage
+    },
+    {
         "id": 1,
         "name": "HackBPIT 2k23",
         "description": "HackBPIT, the flagship hackathon of BPIT, is where innovative ideas meet real-world problem-solving! Hosted by Anveshan, HackBPIT 2023 drew an impressive 100+ teams from across the country, all competing for a total prize pool of 1 Lakh with 50k Cash Prize. This high-energy, 30-hour offline event brought together students, industry mentors, and tech enthusiasts to collaborate, create, and build impactful solutions.Participants faced challenging problem statements across multiple domains, encouraging them to think critically and work together under pressure. The event was not just about coding; it was a platform for learning, mentorship, and networking, offering valuable insights and connections for every attendee.",
@@ -22,25 +36,11 @@ const EventsData= [
         "image": TechnovationImage,
     },
     {
-        "id": 3,
-        "name": "Meet Your Alumni: Sawarnee Sethi",
-        "description": "Meet Your Alumni is a special event by Anveshan, designed to connect BPIT students with accomplished alumni for invaluable career guidance. Recently, we hosted Mr. Sawarnee Sethi, a Cloud Engineer at AWS, who shared his expertise on Internships and Placements.In this session, Mr. Sethi offered strategies for navigating the internship and placement process, preparing for interviews, and building a standout profile. With his firsthand industry insights, students gained practical knowledge on transitioning from academic life to a successful tech career.", 
-        "date": "18-03-2024",
-        "image": AlumImage
-    },
-    {
         "id": 4,
         "name": "Cracking Gate by Unacademy",
         "description": "Anveshan, in collaboration with Unacademy, hosted a focused session on GATE Preparation led by Mr. Gurupal Singh Chawla, a renowned GATE educator at Unacademy. This event provided students with essential insights into the GATE examination, covering preparation strategies, study resources, and tips for excelling in one of the most competitive exams in India.Mr. Chawla shared his expertise on effective time management, key areas to focus on, and how to approach complex topics, giving attendees a solid foundation for their GATE journey. The session was an excellent opportunity for students to gain guidance from a leading educator and enhance their preparation approach.",
         "date": "28-03-2023",
         "image": GateImage
-    },
-    {
-        "id": 5,
-        "name": "Tech Starter 2k24",
-        "description": "Tech Starter is Anveshan's month-long foundational cohort, specially designed to equip students with essential skills across core tech domains. Led by our Core Team members, this immersive program included hands-on sessions on HTML & CSS, Git and GitHub, JavaScript, C++, Java, and an Introduction to Data Structures and Algorithms.Each session was crafted to provide a practical, beginner-friendly approach, allowing students to build confidence and solidify their understanding of these foundational topics. Tech Starter empowers participants with the knowledge and skills they need to embark on their tech journeys, whether they're diving into web development, coding fundamentals, or programming logic.",
-        "date": "14-09-2024 to 17-10-2024",
-        "image": TechStarterImage,
     },
     {
         "id": 6,
